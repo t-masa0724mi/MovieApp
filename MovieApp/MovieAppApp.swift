ba6d128c6b408f0ac69eab5a584a8be3c1514e7d
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-29 17:38:39 +0900

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by 飯塚政美 on 2023/06/29.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
